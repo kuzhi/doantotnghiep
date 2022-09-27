2b227fd416563def471b310adddf9da2efd2d5f0
khangnm <khangnmpc02586@fpt.edu.vn>
2022-09-27 17:25:13 +0700

    Create Entity

---
+++ src/main/java/fpoly/chickens/entity/OrderDetail.java
@@ -0,0 +1,47 @@
+package fpoly.chickens.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "OrderDetail")
+public class OrderDetail {
+	
+	@Id
+	@Column(name = "Id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+	
+
+	@ManyToOne
+	@JoinColumn(name="Productid")
+	Product product;
+	
+	@ManyToOne
+	@JoinColumn(name="Orderid")
+	Order order;
+	
+	@Column(name = "Quanlity")
+	private Integer Quanlity;
+	
+	@Column(name = "Total")
+	private Integer Total;
+	
+}
---
+++ src/main/java/fpoly/chickens/entity/UserApp.java
@@ -0,0 +1,55 @@
+package fpoly.chickens.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Usersapp")
+public class UserApp implements Serializable {
+
+	@Id
+	@Column(name = "Id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+
+	@Column(name = "Username")
+	private String Username;
+
+	@Column(name = "Password")
+	private String Password;
+
+	@Column(name = "Fullname")
+	private String Fullname;
+
+	@Column(name = "Gender")
+	private Boolean Gender;
+
+	@Column(name = "Email")
+	private String Email;
+
+	@Column(name = "Birthday")
+	private Date Birthday;
+
+	@Column(name = "Citizenidentitycard")
+	private String Citizenidentitycard;
+
+	@Column(name = "Deleted")
+	private Boolean Deleted;
+}
