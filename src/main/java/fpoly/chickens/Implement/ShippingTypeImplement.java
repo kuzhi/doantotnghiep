1afa45066b6f1f842e7eeca194becb492d7fd5da
khangnm <khangnmpc02586@fpt.edu.vn>
2022-09-28 10:25:48 +0700

    Create Implement

---
+++ src/main/java/fpoly/chickens/Implement/ShippingTypeImplement.java
@@ -0,0 +1,12 @@
+package fpoly.chickens.Implement;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+import fpoly.chickens.service.ShippingTypeService;
+
+@SessionScope
+@Service
+public class ShippingTypeImplement implements ShippingTypeService{
+
+}
---
+++ src/main/java/fpoly/chickens/Implement/StoreImplement.java
@@ -0,0 +1,11 @@
+package fpoly.chickens.Implement;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+import fpoly.chickens.service.StoreService;
+
+@SessionScope
+@Service
+public class StoreImplement implements StoreService {
+
+}
