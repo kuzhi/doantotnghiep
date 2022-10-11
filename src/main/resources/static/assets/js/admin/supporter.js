6b972478c2bb5010f15c0070de2605c4de859433
Link2k2 <linhhc1612gen@gmail.com>
2022-10-11 15:32:57 +0700

    Finish admin

--- src/main/resources/static/assets/js/admin/supporter.js
+++ src/main/resources/static/assets/js/admin/supporter.js
@@ -13,5 +13,132 @@ app.controller("supporter-ctrl", function($scope, $http, $location) {
 		
 	}
 	
+	$scope.list = {
+		suporter: [
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+			{
+				id: '1000001',
+				name: 'S-Chanel',
+				username: 's-user',
+				img: 'icon_food.png'
+			},
+		],
+		
+		user: [
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+			{
+				id: 1,
+				name: 'Nguyễn Văn An'
+			},
+		]
+	}
 	
 })
