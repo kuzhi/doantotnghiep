7cf46d10566e0e027a413489381640117a17194b
Nguyễn Hoàng Vũ <112169629+kevinvu66@users.noreply.github.com>
2022-11-13 22:19:03 +0700

    Admin_QuanLyNguoiDung_NhanVien (#18)
    Co-authored-by: vunh662002 <kevinvu06062002@gmail.com>

--- src/main/java/fpoly/chickens/dao/UserDAO.java
+++ src/main/java/fpoly/chickens/dao/UserDAO.java
@@ -1,10 +1,34 @@
 package fpoly.chickens.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import fpoly.chickens.entity.Product;
 import fpoly.chickens.entity.User;
 
 
 public interface UserDAO extends JpaRepository<User, Integer>{
+	@Query("SELECT o FROM User o WHERE o.Deleted = ?1")
+	List<User> loadUserWithDeleted(Boolean deleted);
+	
+	@Query(value = "SELECT o FROM User o WHERE o.Fullname LIKE ?1")
+	List<User> findByName(String name);
+	
+	// Sort A-Z
+		@Query(value = "SELECT o FROM User o ORDER BY o.Fullname ASC")
+		List<User> sortAZ();
 
+		// Sort A-Z
+		@Query(value = "SELECT o FROM User o ORDER BY o.Fullname DESC")
+		List<User> sortZA();
+		
+		// Sort A-Z
+		@Query(value = "SELECT o FROM User o WHERE o.Status = true")
+		List<User> hoatDong();
+		
+		// Sort A-Z
+		@Query(value = "SELECT o FROM User o WHERE o.Status = false")
+		List<User> ngungHoatDong();
 }
