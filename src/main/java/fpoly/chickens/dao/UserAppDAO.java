5be5ec97d702336aab696c691e94123b484c636d
vunh662002 <kevinvu06062002@gmail.com>
2022-11-15 19:44:20 +0700

    Admin_QuanLyNhanVien

--- src/main/java/fpoly/chickens/dao/UserAppDAO.java
+++ src/main/java/fpoly/chickens/dao/UserAppDAO.java
@@ -1,8 +1,39 @@
 package fpoly.chickens.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import fpoly.chickens.entity.User;
 import fpoly.chickens.entity.UserApp;
 
 public interface UserAppDAO extends JpaRepository<UserApp, Integer>{
+	@Query("SELECT o FROM UserApp o WHERE o.Deleted = ?1")
+	List<UserApp> loadUserWithDeleted(Boolean deleted);
+	
+	@Query(value = "SELECT o FROM UserApp o WHERE o.Fullname LIKE ?1")
+	List<UserApp> findByName(String name);
+	
+	@Query(value = "SELECT o FROM UserApp o WHERE o.Username LIKE ?1")
+	List<UserApp> findUserByUserName(String name);
+	
+	@Query(value = "SELECT o FROM UserApp o WHERE o.Username LIKE ?1 AND o.Fullname LIKE ?1")
+	List<UserApp> findUserByUserNameAndFullName(String username, String fullname);
+	
+	// Sort A-Z
+		@Query(value = "SELECT o FROM UserApp o ORDER BY o.Fullname ASC")
+		List<UserApp> sortAZ();
 
+		// Sort A-Z
+		@Query(value = "SELECT o FROM UserApp o ORDER BY o.Fullname DESC")
+		List<UserApp> sortZA();
+		
+		// Sort A-Z
+		@Query(value = "SELECT o FROM UserApp o WHERE o.Gender = true")
+		List<UserApp> hoatDong();
+		
+		// Sort A-Z
+		@Query(value = "SELECT o FROM UserApp o WHERE o.Gender = false")
+		List<UserApp> ngungHoatDong();
 }
