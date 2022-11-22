e06c22abec91f7782360496fe8d4e7158bf4a45b
vunh662002 <kevinvu06062002@gmail.com>
2022-11-22 11:48:25 +0700

    a

--- src/main/java/fpoly/chickens/dao/UserAppDAO.java
+++ src/main/java/fpoly/chickens/dao/UserAppDAO.java
@@ -8,15 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import fpoly.chickens.entity.User;
 import fpoly.chickens.entity.UserApp;
 
-<<<<<<< HEAD
-public interface UserAppDAO extends JpaRepository<UserApp, Integer> {
-	@Query(value = "SELECT * FROM UserApp WHERE username = ?1", nativeQuery = true)
-	public UserApp findByUsername(String uname);
-
-	@Query("SELECT o FROM UserApp o WHERE o.Deleted = ?1")
-	List<UserApp> loadUserWithDeleted(Boolean deleted);
-
-=======
 
 public interface UserAppDAO extends JpaRepository<UserApp, Integer>{
 	@Query("SELECT o FROM UserApp o WHERE o.Deleted = ?1")
@@ -25,7 +16,6 @@ public interface UserAppDAO extends JpaRepository<UserApp, Integer>{
 	@Query(value="SELECT * FROM UserApp WHERE username = ?1", nativeQuery = true)
 	public UserApp findByUsername(String uname);
 	
->>>>>>> 875e749bd960e619642ea240c8a9cd15e60e8342
 	@Query(value = "SELECT o FROM UserApp o WHERE o.Fullname LIKE ?1")
 	List<UserApp> findByName(String name);
 
@@ -35,7 +25,6 @@ public interface UserAppDAO extends JpaRepository<UserApp, Integer>{
 	@Query(value = "SELECT o FROM UserApp o WHERE o.Username LIKE ?1 AND o.Fullname LIKE ?1")
 	List<UserApp> findUserByUserNameAndFullName(String username, String fullname);
 
-<<<<<<< HEAD
 	// Sort A-Z
 	@Query(value = "SELECT o FROM UserApp o ORDER BY o.Fullname ASC")
 	List<UserApp> sortAZ();
@@ -52,17 +41,5 @@ public interface UserAppDAO extends JpaRepository<UserApp, Integer>{
 	@Query(value = "SELECT o FROM UserApp o WHERE o.Gender = false")
 	List<UserApp> ngungHoatDong();
 
-=======
-		// Sort A-Z
-		@Query(value = "SELECT o FROM UserApp o ORDER BY o.Fullname DESC")
-		List<UserApp> sortZA();
-		
-		// Sort A-Z
-		@Query(value = "SELECT o FROM UserApp o WHERE o.Gender = true")
-		List<UserApp> hoatDong();
-		
-		// Sort A-Z
-		@Query(value = "SELECT o FROM UserApp o WHERE o.Gender = false")
-		List<UserApp> ngungHoatDong();
->>>>>>> 875e749bd960e619642ea240c8a9cd15e60e8342
+
 }
