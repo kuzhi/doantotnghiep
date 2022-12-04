b1944494897b5fa1b360c47bc73b898ea0425446
Link2k2 <linhhc1612gen@gmail.com>
2022-12-04 23:24:05 +0700

    add select store

--- src/main/resources/static/assets/js/user_store/name__store-ctrl.js
+++ src/main/resources/static/assets/js/user_store/name__store-ctrl.js
@@ -2,9 +2,6 @@ app.controller("name__store-ctrl", function($scope, $http, $location) {
 	$scope.regexPhone = /^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/;
 	$scope.titleBread = 'Chi tiết cửa hàng';
 	$scope.titleBreadcrumb = 'Thông tin chung'
-	$scope.nameStore = "Pika Tea";
-	
-
 
 	$scope.listStoreByStoreId = [];
 	$scope.listStoreByUserId = [];
